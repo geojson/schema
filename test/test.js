134833484af0e216c6bebbc887a4ca442e531f49
Tim Schaub <tim.schaub@gmail.com>
2016-04-30 18:06:37 -0600

    LineString fixtures

--- test/test.js
+++ test/test.js
@@ -3,5 +3,8 @@ const assert = require('./assert');
 assert.valid('Point.json', 'valid/point/**/*.json');
 assert.invalid('Point.json', 'invalid/point/**/*.json');
 
+assert.valid('LineString.json', 'valid/linestring/**/*.json');
+assert.invalid('LineString.json', 'invalid/linestring/**/*.json');
+
 assert.valid('GeoJSON.json', 'valid/**/*.json');
 assert.invalid('GeoJSON.json', 'invalid/**/*.json');
