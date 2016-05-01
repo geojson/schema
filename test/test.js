8eedefe611e5c0ccbf0b763843e275e25f3fffbb
Tim Schaub <tim.schaub@gmail.com>
2016-05-01 08:19:09 -0600

    Polygon test fixtures

--- test/test.js
+++ test/test.js
@@ -6,5 +6,8 @@ assert.invalid('Point.json', 'invalid/point/**/*.json');
 assert.valid('LineString.json', 'valid/linestring/**/*.json');
 assert.invalid('LineString.json', 'invalid/linestring/**/*.json');
 
+assert.valid('Polygon.json', 'valid/polygon/**/*.json');
+assert.invalid('Polygon.json', 'invalid/polygon/**/*.json');
+
 assert.valid('GeoJSON.json', 'valid/**/*.json');
 assert.invalid('GeoJSON.json', 'invalid/**/*.json');
