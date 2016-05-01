4f4198aa654bf91642aef62047f5472bdb7dbf04
Tim Schaub <tim.schaub@gmail.com>
2016-05-01 08:21:44 -0600

    Assertions of invalidity

--- test/test.js
+++ test/test.js
@@ -1,13 +1,17 @@
 const assert = require('./assert');
 
 assert.valid('Point.json', 'valid/point/**/*.json');
-assert.invalid('Point.json', 'invalid/point/**/*.json');
-
 assert.valid('LineString.json', 'valid/linestring/**/*.json');
-assert.invalid('LineString.json', 'invalid/linestring/**/*.json');
-
 assert.valid('Polygon.json', 'valid/polygon/**/*.json');
-assert.invalid('Polygon.json', 'invalid/polygon/**/*.json');
-
 assert.valid('GeoJSON.json', 'valid/**/*.json');
+
+assert.invalid('Point.json', 'invalid/**/*.json');
+assert.invalid('LineString.json', 'invalid/**/*.json');
+assert.invalid('Polygon.json', 'invalid/**/*.json');
+assert.invalid('MultiPoint.json', 'invalid/**/*.json');
+assert.invalid('MultiLineString.json', 'invalid/**/*.json');
+assert.invalid('MultiPolygon.json', 'invalid/**/*.json');
+assert.invalid('GeometryCollection.json', 'invalid/**/*.json');
+assert.invalid('Feature.json', 'invalid/**/*.json');
+assert.invalid('FeatureCollection.json', 'invalid/**/*.json');
 assert.invalid('GeoJSON.json', 'invalid/**/*.json');
